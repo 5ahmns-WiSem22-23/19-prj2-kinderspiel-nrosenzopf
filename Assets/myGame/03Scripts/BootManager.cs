81fc07afae3c483363c190587553466ee9dc81b6
NadineRosenzopf <nadine.rosenzopf02@htl-salzburg.ac.at>
2023-01-24 19:35:57 +0100

    Goal

--- Assets/myGame/03Scripts/BootManager.cs
+++ Assets/myGame/03Scripts/BootManager.cs
@@ -10,26 +10,33 @@ public class BootManager : MonoBehaviour
     public GameObject fischblau;
     public GameObject fischgelb;
 
+    public float speed = 10f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == fischorange)
         {
             fischorange.SetActive(false);
+          //  fischorange.transform.position += new Vector3(3f, 0, 0);
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
         }
 
         if (other.gameObject == fischrosa)
         {
             fischrosa.SetActive(false);
+            fischrosa.transform.position += new Vector3(3f, 0, 0);
         }
 
         if (other.gameObject == fischblau)
         {
             fischblau.SetActive(false);
+            fischblau.transform.position += new Vector3(30f, 0, 0);
         }
 
         if (other.gameObject == fischgelb)
         {
             fischgelb.SetActive(false);
+            fischgelb.transform.position += new Vector3(3f, 0, 0);
         }
     }
 }
--- Assets/myGame/03Scripts/Goal.cs
+++ Assets/myGame/03Scripts/Goal.cs
@@ -10,27 +10,37 @@ public class Goal : MonoBehaviour
     public GameObject fischblau;
     public GameObject fischgelb;
 
+    public GameObject bubblesorange;
+    public GameObject bubblesrosa;
+    public GameObject bubblesblau;
+    public GameObject bubblesgelb;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == fischorange)
         {
             Debug.Log("Goal");
+            bubblesorange.SetActive(true);
+
         }
 
         if (other.gameObject == fischrosa)
         {
             Debug.Log("Goal");
+            bubblesrosa.SetActive(true);
         }
 
         if (other.gameObject == fischblau)
         {
             Debug.Log("Goal");
+            bubblesblau.SetActive(true);
         }
 
         if (other.gameObject == fischgelb)
         {
             Debug.Log("Goal");
+            bubblesgelb.SetActive(true);
         }
     }
 
