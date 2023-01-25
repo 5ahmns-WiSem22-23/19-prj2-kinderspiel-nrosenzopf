fc59e8c33d72646079e299af5aada2f13cd04503
NadineRosenzopf <nadine.rosenzopf02@htl-salzburg.ac.at>
2023-01-25 11:05:33 +0100

    2 oder mehrere Fische deaktiviert? Dann GameOver

--- Assets/myGame/03Scripts/BootManager.cs
+++ Assets/myGame/03Scripts/BootManager.cs
@@ -10,35 +10,54 @@ public class BootManager : MonoBehaviour
     public GameObject fischblau;
     public GameObject fischgelb;
 
-
+    public int deactivatedGO = 0;
+    public bool x = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == fischorange)
         {
+            deactivatedGO++;
             fischorange.SetActive(false);
         }
 
         if (other.gameObject == fischrosa)
         {
-           fischrosa.SetActive(false);
+            deactivatedGO++;
+            fischrosa.SetActive(false);
 
         }
 
         if (other.gameObject == fischblau)
         {
+            deactivatedGO++;
             fischblau.SetActive(false);
         }
 
         if (other.gameObject == fischgelb)
         {
+            deactivatedGO++;
             fischgelb.SetActive(false);
         }
+
+
+
 
 
     //   if(other.gameObject == fischorange && other.gameObject == fischrosa || other.gameObject == fischorange && other.gameObject == fischblau || other.gameObject == fischorange &&  other.gameObject == fischgelb || other.gameObject == fischrosa && other.gameObject == fischblau || other.gameObject == fischrosa && other.gameObject == fischgelb)
      //   {
     //        Debug.Log("Zwei");
      //   } 
+    }
+
+
+    void Update()
+    {
+        
+        if (deactivatedGO >= 2 && x)
+        {
+            Debug.Log("Zwei oder mehr Gameobjekte wurden deaktiviert.");
+            x = false;
+        }
     }
 }
